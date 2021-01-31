23ab9ffe03977dfc88b1f2a85939961e92b37f5a
Nadia Smirnykh <nadianizh0gmail.com>
2021-01-31 15:09:53 +0200

    some variables name changed

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -9,40 +9,40 @@ fun main() {
 }
 
 fun applyDiscount(amountSpent: Int, isMusicFan: Boolean, amount: Int) {
-    val discount0: Int
-    val discount1: Double
-    var discount2 = 0.0
-    var string1 = ""
-    var string2 = ""
-    var string3 = ""
+    val discount100R: Int
+    val discount5percent: Double
+    var discountMusicFun = 0.0
+    var commentDiscount100R  = ""
+    var commentDiscount5percent = ""
+    var commentDiscountMusicFun = ""
     val payRubl: Int
     val payCopec: Int
 
     if (isMusicFan) {
-        discount2 = 0.01
-        string3 = "Применена скидка 'меломан' 1%."
+        discountMusicFun = 0.01
+        commentDiscountMusicFun = "Применена скидка 'меломан' 1%."
     }
 
     if (amountSpent < 1000) {
-        discount0 = 0
-        discount1 = 0.0
+        discount100R = 0
+        discount5percent = 0.0
     } else if (amountSpent > 1000 && amountSpent < 10001) {
-        discount0 = 100
-        discount1 = 0.0
-        string1 = "Применена скидка 100р."
+        discount100R = 100
+        discount5percent = 0.0
+        commentDiscount100R = "Применена скидка 100р."
     } else {
-        discount0 = 0
-        discount1 = 0.05
-        string2 = "Применена скидка 5%."
+        discount100R = 0
+        discount5percent = 0.05
+        commentDiscount5percent = "Применена скидка 5%."
     }
 
-    val pay = (amount * (1 - discount1) - discount0) * (1 - discount2)
+    val pay = (amount * (1 - discount5percent) - discount100R) * (1 - discountMusicFun)
 
     payRubl = pay.toInt()
     payCopec = ((pay - pay.toInt()) * 100).toInt()
 
     println(
-        "Покупка $amount р. \n $string1 $string2 $string3 " +
+        "Покупка $amount р. \n $commentDiscount100R $commentDiscount5percent $commentDiscountMusicFun " +
                 "Окончательная сумма к оплате: $payRubl р. $payCopec коп."
     )
 }
